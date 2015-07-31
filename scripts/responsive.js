7958df2f4483f433f798b5f47866a0330b833432
Mircea Tanasă <tanasamircea@yahoo.com>
2015-07-31 13:30:22 +0300

    *Responsive navigation script remade in a simpler way

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -1,17 +1,10 @@
 $(document).ready(function() {
-  var flag=0;
-  $('.navigation_responsive').click(function() {
-    $(this).toggleClass('active');
-    if(flag==0){
-      $('.navigation ul').slideDown( 400 );
-      flag=1;
-    }
-    else{
-      $('.navigation ul').slideUp( 400 );
-      flag=0;
-    }
+    $('.navigation_responsive').click(function() {
+        $(this).toggleClass('active');
+        $('.navigation ul').slideToggle( 400 );
     });
-   window.addEventListener('resize', function(event){
+
+    window.addEventListener('resize', function(event){
     if($(window).width()>700){
     $('.navigation ul').css('display','inline-block')
     }
