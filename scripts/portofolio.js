69ea243e18dcda33e27673946b1750cb4afa3ad5
Mircea Tanasă <tanasamircea@yahoo.com>
2015-08-06 00:09:10 +0300

    *First version of portfolio with project selecting feature

--- scripts/portofolio.js
+++ scripts/portofolio.js
@@ -1,9 +1,16 @@
   $(document).ready(function(){
-    var that = this;
-    var filters=('.portofolio_chooser div')
-    var gallery=('.portofolio_gallery');
 
-    $(filters).click(function() {
+    var that = this;
+    var filters=('.portofolio_chooser div');
+    var gallery=('.portofolio_gallery');
+    var project=('.portofolio_gallery div')
+
+    var displayer=('.project_displayer');
+    var btnClose=('.close-button');
+    var projInfo=('.project-displayer_info');
+
+
+  $(filters).click(function() {
       var ourClass = $(this).attr('class');
       $(filters).removeClass('active');
       $(this).addClass('active');
@@ -14,5 +21,21 @@
         $(gallery).children(':not(.project-'+ourClass+')').hide("slow");
         $(gallery).children('.project-' + ourClass).show("slow");
         }
+    })
+
+    $(project).click(function(){
+        var info=$(this).attr('info');
+        console.log(info);
+        $(this).clone().appendTo($(displayer));
+        $(displayer).css("display","block");
+        $(displayer).children().eq(1).addClass('displayed');
+        $('<div class="project-displayer_info">'+info+'</div>').appendTo($(displayer));
+      });
+    $(btnClose).click(function(){
+      $(displayer).children().eq(1).remove();
+      $(displayer).children(projInfo).remove();
+      $(displayer).css("display","none");
     });
+
+
   });
